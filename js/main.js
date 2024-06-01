4e6258345550003e616c540e449b2531ad7ec323
piojo55 <ocattoni@gmail.com>
2024-06-01 15:03:43 -0300

    UP

--- js/main.js
+++ js/main.js
@@ -1,61 +1,53 @@
 // IMAGES CARD
 let products = [
     {
-        "nombre": "Pastel de chocolate",
-        "descripcion": "Delicioso pastel de chocolate con cobertura de ganache.",
-        "precio": 350,
-        "imagen1": "product.jpg",
-        "imagen2": "product-2.jpg",
-        "imagen3": "pastel_chocolate-2.jpg",
-        "off": "20%"
+        "nombre": "Tarta de Manzana",
+        "descripcion": "Deliciosas manzanas frescas, canela aromática, azúcar moreno y una base de masa hojaldrada.",
+        "precio": "25.350,00",
+        "imagen1": "TartaDeManzana.jpg",
+        "imagen2": "TartaDeManzana2.jpg"
     },
     {
         "nombre": "Pastel de chocolate",
-        "descripcion": "Delicioso pastel de chocolate con cobertura de ganache.",
-        "precio": 350,
-        "imagen1": "product.jpg",
-        "imagen2": "product-2.jpg",
-        "imagen3": "pastel_chocolate-2.jpg"
+        "descripcion": "Super Cacao, harina de alta calidad, azúcar refinada y mantequilla fresca.",
+        "precio": "29.420,00",
+        "imagen1": "TartaDeChocolate.jpg",
+        "imagen2": "TartaDeChocolate2.jpg"
     },
     {
         "nombre": "Tarta de queso",
-        "descripcion": "Tarta de queso cremosa con base de galleta y mermelada de frutos rojos.",
-        "precio": 300,
-        "imagen1": "product.jpg",
-        "imagen2": "product-2.jpg",
-        "imagen3": "pastel_chocolate-2.jpg"
+        "descripcion": "Queso crema suave, azúcar refinada, huevos frescos y una base de galleta crujiente",
+        "precio": "32.570.00",
+        "imagen1": "TartaDeQueso.jpg",
+        "imagen2": "TartaDeQueso2.jpg",
     },
     {
-        "nombre": "Croissants",
-        "descripcion": "Croissants de mantequilla, recién horneados y crujientes.",
-        "precio": 200,
-        "imagen1": "product.jpg",
-        "imagen2": "product-2.jpg",
-        "imagen3": "pastel_chocolate-2.jpg"
+        "nombre": "Muffins",
+        "descripcion": "Super esponjosos con huevos frescos, mantequilla cremosa, azúcar refinada y un toque de vainilla natural.",
+        "precio": "12.000,00",
+        "imagen1": "Muffins.jpg",   
+        "imagen2": "Muffins2.jpg"
     },
     {
-        "nombre": "Macarons",
-        "descripcion": "Macarons franceses en una variedad de sabores y colores.",
-        "precio": 250,
-        "imagen1": "product.jpg",
-        "imagen2": "product-2.jpg",
-        "imagen3": "pastel_chocolate-2.jpg"
+        "nombre": "Donas",
+        "descripcion": "Irresistible combinación de textura esponjosa por dentro y crujiente por fuera, junto con la dulzura tentadora del glaseado que la cubre.",
+        "precio": "12.000,00",
+        "imagen1": "Donas.jpg",
+        "imagen2": "Donas2.jpg"
     },
     {
-        "nombre": "Tarta de limón",
-        "descripcion": "Tarta de limón con merengue italiano y base de masa quebrada.",
-        "precio": 300,
-        "imagen1": "product.jpg",
-        "imagen2": "product-2.jpg",
-        "imagen3": "pastel_chocolate-2.jpg"
+        "nombre": "Pastel de Fresa",
+        "descripcion": "Extraordinaria combinación de suavidad y frescura en cada bocado, junto con el sabor dulce y refrescante de las fresas frescas.",
+        "precio": "35.870,00",
+        "imagen1": "PastelDeFresa.jpg",
+        "imagen2": "PastelDeFresa2.jpg"
     },
     {
-        "nombre": "Macarons",
-        "descripcion": "Macarons franceses en una variedad de sabores y colores.",
-        "precio": 250,
-        "imagen1": "product.jpg",
-        "imagen2": "product-2.jpg",
-        "imagen3": "pastel_chocolate-2.jpg"
+        "nombre": "Turron",
+        "descripcion": "Deliciosa mezcla de textura suave y crujiente, junto con sus sabores tradicionales.",
+        "precio": "7.000,00",
+        "imagen1": "turron.jpg",
+        "imagen2": "turron2.jpg",
     },
     {
         "nombre": "Tarta de limón",
